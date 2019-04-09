20196725ee46f542380055f1ab2194b8f0778795
unknown <suarmin.rais@gmail.com>
2019-04-09 09:13:32 -0700

    powe

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TextInput, View } from 'react-native';
 
 //props sederhana render nama
 
@@ -50,20 +50,44 @@ const styles = StyleSheet.create({
 
 
 // main program
+// export default class HelloApp extends Component {
+//   render() {
+//     let data = {
+//       img: {
+//         uri:'https://pbs.twimg.com/profile_images/2679996558/e345c52a29da0414a5b01fd48d62df72_400x400.jpeg'
+//       }
+//     };
+//     return (
+//       <View style = {{ backgroundColor:'gray', height:720 }}>
+//         <Image source={data.img} style={{width: 360, height: 320}} />
+//         <Nama isi='INREADY' />
+//         <Blink style={styles.merah} kedip='Worgrup' />
+//         <Nama style={styles.biru} isi='Bede' />
+//       </View>
+//     );
+//   }
+// }
+
 export default class HelloApp extends Component {
+  constructor(props){
+    super(props);
+    this.state = { text:'' }
+  }
+
   render() {
-    let data = {
-      img: {
-        uri:'https://pbs.twimg.com/profile_images/2679996558/e345c52a29da0414a5b01fd48d62df72_400x400.jpeg'
-      }
-    };
     return (
-      <View style = {{ backgroundColor:'gray', height:720 }}>
-        <Image source={data.img} style={{width: 360, height: 320}} />
-        <Nama isi='INREADY' />
-        <Blink style={styles.merah} kedip='Worgrup' />
-        <Nama style={styles.biru} isi='Bede' />
+      <View style={{ padding:10 }}>
+        <TextInput
+          placeholder='Isi Bosku ...'
+          style= {{ height:40 }}
+          onChangeText={ (text) => this.setState({text}) }
+        />
+        <Text style={{ padding:10, fontSize:40 }} >
+        {
+          this.state.text.split(' ').map((a) => a && '🍕').join(' ')
+        }
+        </Text>
       </View>
-    );
+    )
   }
 }
